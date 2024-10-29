f8ef7c5e69ee0d6502bd98711a1e4fa2f9dfd709
Bonny07 <111042029+Bonny07@users.noreply.github.com>
2024-10-30 00:27:56 +0800

    Finish all the register and login

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('ajax/product/create/', views.product_create_ajax, name='product_create_ajax'),
     path('ajax/product/update/', views.product_update_ajax, name='product_update_ajax'),
     path('ajax/product/delete/', views.product_delete_ajax, name='product_delete_ajax'),
