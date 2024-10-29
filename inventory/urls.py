fe734b954c9be22e2fe3c7e30408e9161760c4c6
Bonny07 <111042029+Bonny07@users.noreply.github.com>
2024-10-30 00:49:01 +0800

    Version 1.0

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,4 +1,4 @@
-# inventory/urls.py
+# grocery_store/inventory/urls.py
 
 from django.urls import path
 from . import views
@@ -12,4 +12,8 @@ urlpatterns = [
     path('ajax/product/update/', views.product_update_ajax, name='product_update_ajax'),
     path('ajax/product/delete/', views.product_delete_ajax, name='product_delete_ajax'),
     path('ajax/product/list/', views.product_list_ajax, name='product_list_ajax'),
+    path('personnel/', views.personnel_management, name='personnel_management'),  # 人员管理路径
+    path('ajax/personnel/list/', views.personnel_list_ajax, name='personnel_list_ajax'),  # AJAX 获取人员列表
+    path('ajax/personnel/update/', views.personnel_update_ajax, name='personnel_update_ajax'),  # AJAX 更新人员
+    path('ajax/personnel/delete/', views.personnel_delete_ajax, name='personnel_delete_ajax'),  # AJAX 删除人员
 ]
